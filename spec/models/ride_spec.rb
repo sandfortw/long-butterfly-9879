afa2e67819de7b9c7ee031fd97bd6c3c8c059c89
Weston Sandfort <sandfortw@gmail.com>
2023-02-27 20:47:40 -0700

    create model test for average_mech_exp for a ride

--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -10,5 +10,19 @@ RSpec.describe Ride, type: :model do
     it { should validate_presence_of :open}
     it { should validate_numericality_of :thrill_rating}
   end
+
+  describe '#average_mech_exp' do
+    before do
+      @amusement_park = AmusementPark.create!(name: "Siz Flags Over Texas", admission_cost: 40)
+      @mechanic1 = Mechanic.create!(name: 'Charles', years_experience: 4)
+      @mechanic2 = Mechanic.create!(name: 'Mary', years_experience: 10)
+      @ride1 = Ride.create!(name: 'Texas Titan', thrill_rating: 10, open: true, amusement_park_id: @amusement_park.id)
+      RideMechanic.create!(mechanic_id: @mechanic1.id, ride_id: @ride1.id)
+      RideMechanic.create!(mechanic_id: @mechanic2.id, ride_id: @ride1.id)
+    end
+    it 'should return the average mechanic experience' do
+      expect(@ride1.average_mech_exp).to eq(7)
+    end
+  end
 end
 
